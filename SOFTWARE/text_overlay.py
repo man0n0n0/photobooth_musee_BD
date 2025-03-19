1753869f93102da38667008f01efb979a1edd4d5
camporini@protonmail.com <manoah.camporini@erg.school>
2025-03-19 20:25:53 +0100

    rework

--- SOFTWARE/text_overlay.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-def add_text_to_image(image, text, position=(50, 50), 
-                      font_scale=1, color=(255, 0, 0), thickness=2):
-
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(image, text, position, font, 
-                font_scale, color, thickness, cv2.LINE_AA)
-    return image

7148b1b131b3c36f029a900a0ea5fdc2c36b8119
man0n0n0 <camporini@protonmail.com>
2025-03-20 09:38:27 +0000

    onreboot new bdmusee

--- on_reboot.sh
+++ on_reboot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 export DISPLAY=:0
-source /home/pi-bd/Documents/photobooth_musee_BD/venv/bin/activate
-python3 /home/pi-bd/Documents/photobooth_musee_BD/main.py
+export XAUTHORITY=/home/pibd/.Xauthority
+xhost +SI:Localuser:pi-bd
+
+cd /home/pi-bd/Documents/photobooth_musee_bd
+source venv/bin/activate
+python3 main.py >> /home/pi-bd/log_cron.txt 2>&1
